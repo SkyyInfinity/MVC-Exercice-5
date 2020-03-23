a892abb546230b9374146aacd80520e9f49e0218
Dylan Hautecoeur <dylan.hautecoeur@gmail.com>
2020-03-23 12:14:34 +0100

    init

---
+++ config/routes.php
@@ -0,0 +1,8 @@
+<?php
+
+$routes = array(
+    array('home','default','index'),
+
+    //ABONNES
+    array('addabonnes','abonnes','add'),
+);
---
+++ view/app/abonnes/add.php
@@ -0,0 +1,3 @@
+<form action="" method="post">
+  <?php echo $form->label('nom') ?>
+</form>
